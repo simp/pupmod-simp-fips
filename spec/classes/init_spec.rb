08335ffc3b9113cc67fcac95ff5594b19f760dbd
Trevor Vaughan <tvaughan@onyxpoint.com>
2017-05-05 08:11:00 -0500

    (SIMP-3138) Fix non-partition /boot
    This works around an undocumented issue where /boot is not on its own
    paritition but is added to the kernel parameters with fips=1.
    
    This situation ends up creating a kernel panic on the system until the
    boot= entry is removed from the kernel boot line.
    
    Added tests against GCE since that is the default image configuration
    on those systems.
    
    SIMP-3138 #close

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -5,7 +5,8 @@ describe 'fips' do
     on_supported_os.each do |os, facts|
       let(:facts){
         facts.merge({
-        :boot_dir_uuid => '123-456-789'
+        :root_dir_uuid => '123-456-789',
+        :boot_dir_uuid => '123-456-790'
       }) }
 
       context "on #{os}" do
@@ -25,15 +26,35 @@ describe 'fips' do
             is_expected.to create_package('dracut-fips').that_notifies('Exec[dracut_rebuild]')
             is_expected.to create_package('fipscheck').with_ensure('latest')
           }
-          it { is_expected.to create_kernel_parameter('boot').with_value("UUID=123-456-789") }
+          it { is_expected.to create_kernel_parameter('boot').with_value("UUID=123-456-790") }
           it { is_expected.to create_kernel_parameter('boot').that_notifies('Reboot_notify[fips]') }
           it { is_expected.to create_reboot_notify('fips') }
+
+          context 'when boot is not a separate partition' do
+            let(:facts){
+              facts.merge({
+              :root_dir_uuid => '123-456-789',
+              :boot_dir_uuid => '123-456-789'
+            }) }
+
+            it { is_expected.to compile.with_all_deps }
+            it {
+              is_expected.to create_kernel_parameter('fips').with_value('1')
+              is_expected.to create_kernel_parameter('fips').that_notifies('Reboot_notify[fips]')
+              is_expected.to create_package('dracut-fips').with_ensure('latest')
+              is_expected.to create_package('dracut-fips').that_notifies('Exec[dracut_rebuild]')
+              is_expected.to create_package('fipscheck').with_ensure('latest')
+            }
+            it { is_expected.to_not create_kernel_parameter('boot') }
+            it { is_expected.to create_reboot_notify('fips') }
+          end
         end
 
         context 'when_enabling_fips and aes' do
           let(:facts){ facts.merge({
             :cpuinfo => { :processor0 => { :flags => ['aes'] }},
-            :boot_dir_uuid => '123-456-789'
+            :root_dir_uuid => '123-456-789',
+            :boot_dir_uuid => '123-456-790'
           })}
           let(:params){{
             :enabled => true
@@ -47,7 +68,7 @@ describe 'fips' do
             is_expected.to create_package('dracut-fips-aesni').that_notifies('Exec[dracut_rebuild]')
             is_expected.to create_package('fipscheck').with_ensure('latest')
           }
-          it { is_expected.to create_kernel_parameter('boot').with_value("UUID=123-456-789") }
+          it { is_expected.to create_kernel_parameter('boot').with_value("UUID=123-456-790") }
           it { is_expected.to create_kernel_parameter('boot').that_notifies('Reboot_notify[fips]') }
           it { is_expected.to create_reboot_notify('fips') }
         end
@@ -55,7 +76,8 @@ describe 'fips' do
         context 'when_disabling_fips and aes' do
           let(:facts){ facts.merge({
             :cpuinfo => { :processor0 => { :flags => ['aes'] }},
-            :boot_dir_uuid => '123-456-789'
+            :root_dir_uuid => '123-456-789',
+            :boot_dir_uuid => '123-456-790'
           })}
 
           let(:params){{
